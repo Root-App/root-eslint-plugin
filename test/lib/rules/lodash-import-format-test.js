e86b7cd52bf31d161c11acda5c5d022c8c48e520
Jim Solce <jim.solce@joinroot.com>
2020-01-10 14:28:32 -0500

    Added test cases and fixes

--- test/lib/rules/lodash-import-format-test.js
+++ test/lib/rules/lodash-import-format-test.js
@@ -3,6 +3,7 @@ const RuleTester = require('eslint').RuleTester;
 
 const ruleTester = new RuleTester({ parserOptions: { ecmaVersion: 2018, sourceType: 'module' } });
 const lodashErrorMessage = "Use the explicit import of lodash: `import <name> from 'lodash/<name>';`";
+const lodashWildcardErrorMessage = "Import specific modules from lodash; do not use \"import { _ } from 'lodash'\"";
 const goodFirst = "import first from 'lodash/first';";
 const goodSecond = "import second from 'lodash/second';";
 const goodThird = "import third from 'lodash/third';";
@@ -14,7 +15,10 @@ ruleTester.run('lodash-import-format', rule, {
     "import get from 'lodash/get'",
     "import map from 'lodash/map'",
     "import first from 'lodash/first'",
-    "import first from 'lodash/first';\nimport last from 'lodash/last';"
+    "import first from 'lodash/first';\nimport last from 'lodash/last';",
+    "import { first, second, third, fourth, fifth, sixth } from 'lodash';",
+    "import { _, first, second, third, fourth, fifth, sixth } from 'lodash';",
+    "import { something } from 'somewhere-else';",
   ],
   invalid: [
     {
@@ -26,7 +30,8 @@ ruleTester.run('lodash-import-format', rule, {
       errors: [{ message: lodashErrorMessage }],
     },
     {
-      code: "import { first } from 'lodash';",
+      code: "import assign from 'lodash';",
+      output: "import assign from 'lodash/assign';",
       errors: [{ message: lodashErrorMessage }],
     },
     {
@@ -54,5 +59,25 @@ ruleTester.run('lodash-import-format', rule, {
       output: goodFirst + "\n" + goodSecond + "\n" + goodThird + "\n" + goodFourth + "\n" + goodFifth,
       errors: [{ message: lodashErrorMessage }],
     },
+    {
+      code: "import { get, xor as toggle } from 'lodash';",
+      output: "import get from 'lodash/get';\nimport toggle from 'lodash/xor';",
+      errors: [{ message: lodashErrorMessage }],
+    },
+    {
+      code: "import { get as g, xor as toggle } from 'lodash';",
+      output: "import g from 'lodash/get';\nimport toggle from 'lodash/xor';",
+      errors: [{ message: lodashErrorMessage }],
+    },
+    {
+      code: "import { _, get } from 'lodash';",
+      errors: [{ message: lodashWildcardErrorMessage }]
+    },
+    {
+      code: "import _ from 'lodash';",
+      errors: [{ message: lodashWildcardErrorMessage }]
+    },
+
+
   ],
 });
